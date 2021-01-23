016b875062415638b2bfe37978c0ed426ec162bb
Rajiv Kumar <rajivk@gentrack.com>
2021-01-23 02:25:57 +0000

    adding grid to trending

--- src/components/sections/trending.jsx
+++ src/components/sections/trending.jsx
@@ -1,11 +1,11 @@
-import React from "react"
-import styled from "styled-components"
-import theme from "../../styles/theme"
-import Section from "../../styles/Section"
-import mixins from "../../styles/mixins"
+import React from "react";
+import styled from "styled-components";
+import theme from "../../styles/theme";
+import Section from "../../styles/Section";
+import mixins from "../../styles/mixins";
 import bg from "../../images/landing8.jpg";
 
-const { colors, fonts, fontSizes } = theme
+const { colors, fonts, fontSizes } = theme;
 
 const StyledContainer = styled(Section)`
   background-image: url(${bg});
@@ -20,10 +20,10 @@ const StyledContainer = styled(Section)`
   @media (max-width: 48em) {
     padding-top: 00px;
   }
-  div {
-    width: 100%;
-  }
-`
+  // div {
+  //   width: 100%;
+  // }
+`;
 const StyledTitle = styled.h2`
   // color: ${colors.cream_text};
   height: 600px;
@@ -42,7 +42,7 @@ const StyledTitle = styled.h2`
   @media (max-width: 24em) {
     font-size: 40px;
   }
-`
+`;
 
 const StyledSubtitle = styled.h3`
   color: ${colors.cream_text};
@@ -61,15 +61,41 @@ const StyledSubtitle = styled.h3`
   @media (max-width: 24em) {
     font-size: 10px;
   }
-`
+`;
+
+const StyledWrapper = styled.div`
+  position: absolute; /* or absolute */
+  margin: auto:
+  height: auto;
+  display: grid;
+  grid-template-columns: 250px 250px 250px;
+  grid-gap: 30px;
+  /* background-color: #fff; */
+  color: #444;
+`;
+
+const StyledBox = styled.div`
+  background-color: #444;
+  color: #fff;
+  border-radius: 5px;
+  padding: 20px;
+  font-size: 150%;
+`;
 
 const Trending = () => {
-  
-    return (
-      <StyledContainer id="trending">
-        <StyledTitle>Trending in Auckland</StyledTitle>
-      </StyledContainer>
-    )
-  }
-  
-  export default Trending
+  return (
+    <StyledContainer id="trending">
+      <StyledTitle>Trending in Auckland</StyledTitle>
+      <StyledWrapper>
+        <StyledBox>A</StyledBox>
+        <StyledBox>b</StyledBox>
+        <StyledBox>c</StyledBox>
+        <StyledBox>d</StyledBox>
+        <StyledBox>e</StyledBox>
+        <StyledBox>f</StyledBox>
+      </StyledWrapper>
+    </StyledContainer>
+  );
+};
+
+export default Trending;
